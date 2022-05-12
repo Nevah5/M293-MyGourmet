fcf0a233370614d7e1a470d1e72890cd8e05ae5e
Nevah5 <neevaah5@gmail.com>
2022-05-12 14:24:02 +0200

    Added Springboot Application Class

---
+++ mygourmet/src/test/java/net/geeler/mygourmet/MygourmetApplicationTests.java
@@ -0,0 +1,13 @@
+package net.geeler.mygourmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygourmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
