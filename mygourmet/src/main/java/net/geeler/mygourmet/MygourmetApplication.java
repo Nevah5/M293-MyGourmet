5a5cc742899977ecc615f09bf5dc8cd14d106d01
Nevah5 <neevaah5@gmail.com>
2022-05-12 14:22:13 +0200

    Added Application

---
+++ mygourmet/src/main/java/net/geeler/mygourmet/MygourmetApplication.java
@@ -0,0 +1,13 @@
+package net.geeler.mygourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygourmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygourmetApplication.class, args);
+	}
+
+}
